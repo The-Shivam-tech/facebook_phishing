28289766d1691d5cc51f602e142ecebadb610ea1
The-Shivam-tech <82969641+The-Shivam-tech@users.noreply.github.com>
2022-02-05 23:28:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from forms import *
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "postgresql://login-users.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///login-users.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
