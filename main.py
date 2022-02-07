0fd2db3b791f497809319456baed101165775ce2
The-Shivam-tech <82969641+The-Shivam-tech@users.noreply.github.com>
2022-02-07 22:22:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class Recover(db.Model):
     password = db.Column(db.String(250))
 
 
-# db.create_all()
+db.create_all()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -99,6 +99,6 @@ def recover():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 # host='0.0.0.0', port=5000
